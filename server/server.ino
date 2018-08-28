8cd43541165bf9967aa6aa1775493e86de7cd071
Unknown <ale_antonioli@hotmail.com>
2018-08-28 17:48:47 -0300

    Server update
    Global variables

--- server/server.ino
+++ server/server.ino
@@ -1,11 +1,16 @@
-#include <FS.h>
-
+/*--------------------------------------------------------------------------
+  --------------------------Variáveis globais ajustaveis: ------------------------*/
+int intervalo_tempo; // em milesegundos, para atualizar a temperatura lida 
+//const char* ssid = "";
+//const char* password =  "";
+boolean formato; // false (0) - normal true (1) configuraçao
+/*--------------------------------------------------------------------------
+  --------------------------Configuracao NTP Server ------------------------*/
 #include <NTPClient.h>//Biblioteca do NTP.
 #include <WiFiUdp.h>//Biblioteca do UDP.
 WiFiUDP udp;//Cria um objeto "UDP".
 NTPClient ntp(udp, "a.st1.ntp.br", -3 * 3600, 60000);//Cria um objeto "NTP" com as configurações.
 String hora;//Váriavel que armazenara o horario do NTP.
-/*------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------
   --------------------------Configuracao Sensor DS18B20 ------------------------*/
 #include <OneWire.h>
@@ -24,9 +29,9 @@ const char* ssid = "WifiCasa";
 const char* password =  "84432320";
 /*--------------------------------------------------------------------------
   --------------------------Configuracao Wi-FI Server ------------------------*/
-#include <WiFiServer.h>//Biblioteca que gerencia o uso do TCP.
-WiFiServer servidor(80);//Cria um objeto "servidor" na porta 80 (http).
-WiFiClient cliente;//Cria um objeto "cliente".
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+ESP8266WebServer server(80);
 String html;//String que armazena o corpo do site.
 /*--------------------------------------------------------------------------
   --------------------------Configuracao ServerMQTT ------------------------*/
@@ -80,13 +85,13 @@ void initMQTT(){
 }
 
 void setup() {    
-  Serial.begin(9600);
+  Serial.begin(115200);
   DS18B20.begin();
   initWifi(); 
   initMQTT();
   
   Serial.println(WiFi.localIP());//Printa o IP que foi consebido ao ESP8266 (este ip que voce ira acessar).
-  servidor.begin();//Inicia o Servidor.
+
    
   ntp.begin();//Inicia o NTP.
   ntp.forceUpdate();//Força o Update.
@@ -110,7 +115,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
  
 void loop() {
   client.loop();
-  http();
   temperatura = getTemperatura();
   dtostrf(temperatura, 2, 2, temperaturaString);
 
@@ -121,34 +125,4 @@ void loop() {
   Serial.println(hora);//Printa a hora já formatada no monitor.
 //  client.publish("TemperaturaAtual", hora );  
 }
-void http()//Sub rotina que verifica novos clientes e se sim, envia o HTML.
-{
-   cliente = servidor.available();//Diz ao cliente que há um servidor disponivel.
- 
-   if (cliente == true)//Se houver clientes conectados, ira enviar o HTML.
-   {
-      String req = cliente.readStringUntil('\r');//Faz a leitura do Cliente.
-      Serial.println(req);//Printa o pedido no Serial monitor.
- 
-      if (req.indexOf("/LED") > -1)//Caso o pedido houver led, inverter o seu estado.
-      {
-         digitalWrite(D4, !digitalRead(D4));//Inverte o estado do led.
-      }
- 
-      html = "";//Reseta a string.
-      html += "HTTP/1.1 Content-Type: text/html\n\n";//Identificaçao do HTML.
-      html += "<!DOCTYPE html><html><head><title>ESP8266 WEB</title>";//Identificaçao e Titulo.
-      html += "<meta name='viewport' content='user-scalable=no'>";//Desabilita o Zoom.
-      html += "<style>h1{font-size:2vw;color:black;}</style></head>";//Cria uma nova fonte de tamanho e cor X.
-      html += "<body bgcolor='ffffff'><center><h1>";//Cor do Background
- 
-      //Estas linhas acima sao parte essencial do codigo, só altere se souber o que esta fazendo!
- 
-      html += "<form action='/LED' method='get'>";//Cria um botao GET para o link /LED
-      html += "<input type='submit' value='LED' id='frm1_submit'/></form>";
- 
-      html += "</h1></center></body></html>";//Termino e fechamento de TAG`s do HTML. Nao altere nada sem saber!
-      cliente.print(html);//Finalmente, enviamos o HTML para o cliente.
-      cliente.stop();//Encerra a conexao.
-   }
-}
+
