710c797bf40e3ae86194c4a79458e9a87480a036
Unknown <ale_antonioli@hotmail.com>
2018-08-30 16:17:58 -0300

    Music implements
    Toca musica para identificar que modo de configuração foi acionado

--- server/server.ino
+++ server/server.ino
@@ -1,3 +1,14 @@
+/*Código que toca uma música. Basta colocar as notas da musica em ordem na variavel musica abaixo
+  No fim das notas escreva "Fim"
+  Não use acentos e escreva a primeira letra maiuscula e a segunda minuscula
+  Logo depois defina, tambem em ordem, o tempo de cada nota
+  NOTAS DISPONÍVEIS:
+  Do, Re, Mi, Fa, Sol, La, Si, Do#, Re#, Fa#, Sol#, La#
+*/
+char* musica[] = {"La", "Re", "Fa", "Sol", "La", "Re", "Fa", "Sol", "Mi", "Pausa", "Sol", "Do", "Fa", "Mi", "Sol", "Do", "Fa", "Mi", "Re", "Fim"}; //Game of Thrones
+int duracao[] = {700, 500, 300, 250, 250, 300, 200, 200, 700, 200, 500, 500, 200, 200, 200, 500, 200, 200, 500};
+char* starwars[] = {"La", "Pausa", "La", "Pausa", "La", "Pausa", "Fa", "Do", "La", "Pausa", "Fa", "Do", "La", "Pausa", "Mi", "Pausa", "Mi", "Pausa", "Mi", "Pausa", "Fa", "Do", "Sol", "Pausa", "Fa", "Do", "La", "Pausa", "La", "Pausa", "La", "Pausa", "La", "Pausa", "La", "Pausa", "Sol#", "Pausa", "Sol", "Fa#", "Fa", "Fa#", "Fim"}; //Marcha Imperial
+int dur[] = {400, 100, 400, 100, 400, 100, 300, 200, 300, 100, 300, 200, 300, 200, 400, 100, 400, 100, 400, 100, 300, 300, 200, 100 , 300, 300, 200, 200, 400, 50, 400, 50, 400, 50, 400, 50, 300, 50, 300, 200, 200, 200};
 /*--------------------------------------------------------------------------
   --------------------------VariÃ¡veis globais ajustaveis: ------------------------*/
 int intervalo_tempo; // em milesegundos, para atualizar a temperatura lida
@@ -92,26 +103,44 @@ void rotinaModo() {
   modo = digitalRead(botao);
   // checa se o botão está pressionado
   if (modo == HIGH) {
-    while(1){
-      beep();
-    }
-    Serial.print("botao clicado ");
+    tocar(musica, duracao);
   }
   else {
-    Serial.print("botao desligado ");
   }
 }
 
-void beep(){
+void tocar(char* mus[], int tempo[]) {
+  int tom = 0;
+  for (int i = 0; mus[i] != "Fim"; i++) {
+    if (mus[i] == "Do") tom = 262;
+    if (mus[i] == "Re") tom = 294;
+    if (mus[i] == "Mi") tom = 330;
+    if (mus[i] == "Fa") tom = 349;
+    if (mus[i] == "Sol") tom = 392;
+    if (mus[i] == "La") tom = 440;
+    if (mus[i] == "Si") tom = 494;
+    if (mus[i] == "Do#") tom = 528;
+    if (mus[i] == "Re#") tom = 622;
+    if (mus[i] == "Fa#") tom = 370;
+    if (mus[i] == "Sol#") tom = 415;
+    if (mus[i] == "La#") tom = 466;
+    if (mus[i] == "Pausa") tom = 0;
+    tone(buzzer, tom, tempo[i]);
+    delay(tempo[i]);
+  }
+  noTone(buzzer);
+}
+
+void beep() {
   tone(buzzer, 1500);
   delay(200);
   noTone(buzzer);
 }
 void setup() {
-  rotinaModo();
   pinMode(buzzer, OUTPUT);
   pinMode(botao, INPUT);
   Serial.begin(115200);
+  rotinaModo();
   DS18B20.begin();
   initWifi();
   initMQTT();
